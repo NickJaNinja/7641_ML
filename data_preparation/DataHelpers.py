5a528639bf8fdbf404169fa47149e6eebb650824
Nick Fratto <nfratto3@gatech.edu>
2022-03-10 22:45:42 -0500

    Added many helpers including some to open batches of images

--- data_preparation/DataHelpers.py
+++ data_preparation/DataHelpers.py
@@ -11,7 +11,7 @@ TEST_DIR = DATA_DIR + 'test/'
 NUM_CLASSES = 10
 
 
-def get_training_image_path(driving_class: int = 0):
+def _get_training_class_path(driving_class: int = 0):
     """Gets the path to a specific class of the training images
     
     Args:
@@ -25,6 +25,19 @@ def get_training_image_path(driving_class: int = 0):
     class_folder_path = f'{TRAIN_DIR}c{driving_class}/'
     return class_folder_path
 
+def _get_training_class_paths():
+    """
+    Returns:
+        list[str]: List of paths to the training class directories.
+    """
+    return [_get_training_class_path(id) for id in range(NUM_CLASSES)]
+
+def get_training_image_paths_by_class():
+    return [[f'{c}{i}' for i in os.listdir(c)] for c in _get_training_class_paths()]
+
+def get_training_image_paths_flat():
+    return [f'{c}{i}' for c in _get_training_class_paths() for i in os.listdir(c)]
+
 def get_test_image_path():
     """Gets the path to all test images
 
@@ -33,22 +46,53 @@ def get_test_image_path():
     """
     return TEST_DIR
 
-def get_random_training_image_path():
-    """Gets the path to a random training image in a random class
-
-    Returns:
-        str: Path to the image
-    """
-    random_class = random.randint(0, NUM_CLASSES-1)
-    training_path = get_training_image_path(driving_class=random_class)
-    imgs = [name for name in os.listdir(training_path)]
-
-    num_images_in_class = len(imgs)
-    random_image = imgs[random.randint(0, num_images_in_class-1)]
-    path_to_image = f'{training_path}{random_image}'
-    return path_to_image
-
 def open_image_as_np(path_to_img: str):
+    """
+    Returns:
+        np.array: Opened image
+    """
     img = Image.open(path_to_img)
     img_as_np = np.asarray(img)
     return img_as_np
+
+def get_random_training_image():
+    """Gets a random training image.
+
+    Returns:
+        np.array: The random image
+    """
+    all_image_paths = get_training_image_paths_flat()
+    rand_path = random.choice(all_image_paths)
+    return open_image_as_np(rand_path)
+
+def get_num_training_images():
+    """
+    Returns:
+        int: Number of training images
+    """
+    return len(get_training_image_paths_flat())
+
+def generate_training_batches(num_batches=200):
+    """Divides training data into batches.
+
+    Returns:
+        list[list[int]]: Batched indices corresponding to images in training data.
+    """
+    num_images = get_num_training_images()
+    image_paths = get_training_image_paths_flat()
+    random.shuffle(image_paths)
+    batch = np.linspace(0, num_images, num_batches+1, dtype=int)
+    return [image_paths[batch[i]:batch[i+1]] for i in range(num_batches)]
+
+def open_training_batch(batch):
+    """Given a batch of paths, opens the whole batch of images and returns them.
+
+    Returns:
+        np.array[image]: Batch of images, shape (N x w x h x 3)
+    """
+    images = []
+    for image_path in batch:
+        img = open_image_as_np(image_path)
+        images.append(img)
+    images = np.array(images)
+    return images
