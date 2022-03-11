2f3ebc22cc0da997a66fbbb01b45a6a95e82c2f0
breannashi <shi00231@umn.edu>
2022-03-11 17:01:53 -0500

    visual

--- analysis/Visualization.py
+++ analysis/Visualization.py
@@ -1,3 +1,20 @@
+import matplotlib.pyplot as plt
 
-def visualize(results):
-    pass
+
+def PCAplot2d(pca):
+    '''
+    Parameters
+    ----------
+    pca : TYPE
+        This will make a 2D pca plot if you run with the output of "get_reduced_data" from Sklearn_PCA.py.
+
+    Returns 2D plot
+    -------
+
+    '''
+    plot = plt.scatter(pca[:,0], pca[:,1])
+    plt.xlabel("PC1")
+    plt.ylabel("PC2")
+    plt.title("First two principal components")
+    plt.show()
+
