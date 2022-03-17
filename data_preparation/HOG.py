fdafee3b1c227554d468a7f23e84e5808d3c163d
Sarah Ge <sarahg@MacBook-Pro-5.local>
2022-03-16 23:12:38 -0400

    modified HOG.py; added HOGplot() function to visualize HOG

--- data_preparation/HOG.py
+++ data_preparation/HOG.py
@@ -9,9 +9,22 @@ class HOG(FeatureExtractor):
         self.pixels_per_cell = pixels_per_cell
 
     def get_features(self, data):
+        '''
+        Args:
+            data: N x H x W x D, where N is the number of images
+        
+        Returns: a tuple of two numpy arrays: (extracted_features, images_after_hog)
+            
+        '''
         features = []
+        hog_images = []
+        
         for image in data:
-            flattened_hog = hog(image, self.orientations,
-                    self.pixels_per_cell, channel_axis=-1)
-            features.append(flattened_hog)
-        return np.array(features)
+
+            flattened_hog = hog(image, self.orientations,self.pixels_per_cell,
+                                cells_per_block=(1, 1), visualize=True,channel_axis=-1)
+
+            features.append(flattened_hog[0])
+            hog_images.append(flattened_hog[1])
+    
+        return (np.array(features),np.array(hog_images))
