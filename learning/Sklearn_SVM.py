7a7bc49c723cfe1b7cc0dffdc218243035e4273f
Nick Fratto <nfratto3@gatech.edu>
2022-03-28 12:45:10 -0400

    SVM predicts probabilities now, consistent with Kaggle

--- learning/Sklearn_SVM.py
+++ learning/Sklearn_SVM.py
@@ -4,10 +4,10 @@ from sklearn import svm
 
 class Sklearn_SVM(Classifier):
     def __init__(self, kernel='linear'):
-        self.classifier = svm.SVC(kernel=kernel)
+        self.classifier = svm.SVC(kernel=kernel, probability=True)
 
     def train(self, x_train, y_train):
         self.classifier.fit(x_train, y_train)
 
     def predict(self, x_predict):
-        return self.classifier.predict(x_predict)
+        return self.classifier.predict_proba(x_predict)
